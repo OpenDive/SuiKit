b94e27e95731aa51771a77cc310e354bbe8446f4
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-24 09:58:10 -0400

    Initial package commit

---
+++ Sources/SuiKit/SuiKit.swift
@@ -0,0 +1,6 @@
+public struct SuiKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SuiKitTests/SuiKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SuiKit
+
+final class SuiKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SuiKit().text, "Hello, World!")
+    }
+}

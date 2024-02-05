49fab86f5c3ed12a653b97cbfacff2fc08943fb4
Marcus Arnett <marcodotio@Marcuss-MacBook-Pro-4.local>
2024-02-05 22:21:15 +0800

    Implement lint for Checkpoint objects

--- Sources/SuiKit/Types/Structs/Checkpoints/CheckpointPage.swift
+++ Sources/SuiKit/Types/Structs/Checkpoints/CheckpointPage.swift
@@ -33,12 +33,18 @@ public struct CheckpointPage {
     /// A string representing the cursor for the next page of checkpoints. This can be used to request the next set of checkpoints in pagination.
     public let nextCursor: String?
 
+    /// Object containing the metadata info regarding the current checkpoint page.
     public let pageInfo: PageInfo?
 
     /// A boolean indicating whether there is another page of checkpoints available after the current page.
     public let hasNextPage: Bool?
 
-    public init(data: [Checkpoint], nextCursor: String? = nil, pageInfo: PageInfo? = nil, hasNextPage: Bool? = nil) {
+    public init(
+        data: [Checkpoint],
+        nextCursor: String? = nil, 
+        pageInfo: PageInfo? = nil,
+        hasNextPage: Bool? = nil
+    ) {
         self.data = data
         self.nextCursor = nextCursor
         self.hasNextPage = hasNextPage
--- Sources/SuiKit/Types/Structs/Checkpoints/PageInfo.swift
+++ Sources/SuiKit/Types/Structs/Checkpoints/PageInfo.swift
@@ -1,16 +1,42 @@
 //
-//  File.swift
-//  
+//  PageInfo.swift
+//  SuiKit
 //
-//  Created by Marcus Arnett on 12/20/23.
+//  Copyright (c) 2024 OpenDive
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 
+/// An object that contains information regarding the current page query from a given requests.
 public struct PageInfo: Equatable {
+    /// The starting point of the array within the context of the entire list of objecs.
     public var startCursor: String?
+
+    /// The ending point of the array within the context of the entire list of objects.
     public var endCursor: String?
+
+    /// Whether or not there is another page ahead of the current page.
     public var hasNextPage: Bool
+
+    /// Whether or not there is another page before the current page.
     public var hasPreviousPage: Bool
 
     public init(graphql: GetCheckpointsQuery.Data.Checkpoints.PageInfo) {

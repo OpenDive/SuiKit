dff468ad02509826602402d43bf58d800a54fcd1
MarcoDotIO <Marcus.Arnett10@gmail.com>
2024-10-01 07:22:11 -0400

    fix: modify Package.swift to incorporate correct language and OS versions

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.5.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SuiKit",
-    platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v7), .tvOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v10_14), .watchOS(.v10), .tvOS(.v17)],
     products: [
         .library(
             name: "SuiKit",
@@ -47,5 +47,5 @@ let package = Package(
             resources: [.copy("Resources")]
         ),
     ],
-    swiftLanguageVersions: [.version("5.9")]
+    swiftLanguageVersions: [.v5]
 )
